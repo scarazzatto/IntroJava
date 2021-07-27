d8afa6e1b5d084d81f3eec5cfcbc027aaeb5b4c5
scarazzatto <fscarazzatto@gmail.com>
2021-07-27 16:39:15 -0300

    Inclusão da 2 métodos sendo chamados pelo método main()

--- src/main/java/intro/Medidas.java
+++ src/main/java/intro/Medidas.java
@@ -8,35 +8,37 @@ public class Medidas {
     //3.1 - Atributos - Características
 
     //3.2 - Métodos e Funções
-    public static void main(String[] args){
-        System.out.println("Bom Dia!!!");
-        System.out.println("Cálculo de àreas");
-
+    public static void main(String[] args) {
+        //calcularAreaModoCurto();
+        calcularAreaModoExtenso();
     }
 
-    public void calcularAreaModoExtenso(){
 
-        // Cálculo de área - Exemplo: o tamanho do tapete ou do piso
-        int largura;
-        int comprimento;
-        int resultado;
-
-        largura = 5; // Largura recebe 4
-        comprimento = 6;
-
-        resultado = largura * comprimento;
-        System.out.println("Para a largura de "+largura+"m e comprimento de "+
-                comprimento+"m a área é de "+resultado + " m²");
-    }
-
-    public void calcularAreaModoCompacto(){
-
+    public static void calcularAreaModoCurto(){
         //Cálculo de área - Reduzido
 
         int largura = 4;
         int comprimento = 3;
 
+        System.out.println("Cálculo de àreas Modo Curto");
         System.out.println("Para a largura de "+largura+"m e comprimento de "+
                 comprimento+"m a área é de "+largura * comprimento + " m²");
     }
-}
+
+        public static void calcularAreaModoExtenso(){
+
+            // Cálculo de área - Exemplo: o tamanho do tapete ou do piso
+            int largura;
+            int comprimento;
+            int resultado;
+
+            largura = 5; // Largura recebe 4
+            comprimento = 6;
+
+            resultado = largura * comprimento;
+            System.out.println("Cálculo de àreas Modo Extenso");
+            System.out.println("Para a largura de "+largura+"m e comprimento de "+
+                    comprimento+"m a área é de "+resultado + " m²");
+        }
+
+    }
